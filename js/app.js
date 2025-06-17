b84071f672e482d77b3adc1021e75891b36e77d7
Sparesotto Anais <anais.camille.sparesotto@gmail.com>
2025-06-17 11:23:54 +0200

    feat: add version in html

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 import { ProgressManager } from './progress.js';
 import { ExportManager } from './export.js';
 import { FilterManager } from './filters.js';
+import { VersionManager } from './version-manager.js';
 
 class App {
     constructor() {
         this.progressManager = new ProgressManager();
         this.exportManager = new ExportManager();
         this.filterManager = new FilterManager();
-
+        this.versionManager = new VersionManager();
+        
+        this.initializeVersion();
+        
         document.addEventListener('checklist-updated', () => {
             this.progressManager.updateProgress();
         });
+    }
+
+    async initializeVersion() {
+        await this.versionManager.loadVersion();
+        const versionElement = document.getElementById('version-number');
+        if (versionElement) {
+            versionElement.textContent = this.versionManager.getFormattedVersion();
+        }
     }
 }
 
